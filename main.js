5c6bb3e9b6281b64fe9a529a8481a11cf3a54440
Patatatat <andres.guluarte@gmail.com>
2022-10-16 12:51:00 -0600

    shopping cart in process

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu')
+const menuShoppingcartIcon = document.querySelector('.navbar-shopping-cart');
+const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuShoppingcartIcon.addEventListener('click', toggleShoppingcartAside)
 
 function toggleDesktopMenu(){
   desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+  mobileMenu.classList.toggle('inactive');
+}
+
+function toggleShoppingcartAside(){
+  aside.classList.toggle('inactive')
 }
