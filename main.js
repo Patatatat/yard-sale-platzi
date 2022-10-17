9eb6a712db2a682dd574310f6dd987e17a9991f8
Patatatat <andres.guluarte@gmail.com>
2022-10-16 22:24:28 -0600

    shoppingcart done

--- main.js
+++ main.js
@@ -7,16 +7,33 @@ const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
-menuShoppingcartIcon.addEventListener('click', toggleShoppingcartAside)
+menuShoppingcartIcon.addEventListener('click', toggleShoppingcartAside);
 
 function toggleDesktopMenu(){
+  const isAsideMenuClosed = aside.classList.contains('inactive')
   desktopMenu.classList.toggle('inactive');
+  if (!isAsideMenuClosed){
+    aside.classList.add('inactive');
+  }
 }
 
 function toggleMobileMenu(){
+  const isAsideMenuClosed = aside.classList.contains('inactive');
   mobileMenu.classList.toggle('inactive');
+  if (!isAsideMenuClosed){
+    aside.classList.add('inactive');
+  }
 }
 
 function toggleShoppingcartAside(){
-  aside.classList.toggle('inactive')
+  const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+  const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+  if (!isMobileMenuClosed || !isDesktopMenuClosed){
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+  }
+
+  aside.classList.toggle('inactive');
+
 }
